7124c7b04c064b377a9f538c06a75937b018ac21
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2025-12-06 11:06:17 +0530

    Initial commit

---
+++ src/main/java/com/tanishq/uber/model/Rider.java
@@ -0,0 +1,13 @@
+package com.tanishq.uber.model;
+
+import javax.xml.crypto.Data;
+
+public class Rider {
+    private String id ;
+    private String userId ;
+    private String driverID ;
+    private String pickupLocation ;
+    private String dropLocation ;
+    private String status ;
+    private Data createdAt ;
+}
